0bc4150177a6741ce7b8f3687ac98c3ee386ea5f
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-09 17:35:54 +0530

    tail

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="w-screen h-screen flex items-center justify-center text-blue-600">
       Food Delivery App
     </div>
   );
