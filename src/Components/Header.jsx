244a79b8b2631dd607d545caa0c4c969734c982a
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-09 20:26:49 +0530

    foodApp-navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,70 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+
+import { motion } from "framer-motion";
+import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
+
+
+
+import Logo from "../img/logo.png";
 
 const Header = () => {
   return (
-    <div
+    <header
     className='fixed z-50 w-screen
-    bg-slate-300 p-6 px-16'>
+     p-6 px-16'>
 
-        <div className='hidden md:flex w-full h-full'></div>
+      <div className="hidden md:flex w-full h-full items-center justify-between">
+        <Link to={"/"} className="flex items-center gap-2">
+          <img src={Logo} className="w-8 object-cover" alt="logo" />
+          <p className="text-headingColor text-xl font-bold"> City</p>
+        </Link>
+
+      <div className="flex items-center gap-8">
+          <motion.ul
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+            className="flex items-center gap-24  ml-auto "
+          >
+
+              
+            <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Home
+            </li>
+            <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              Menu
+            </li>
+            <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              About Us
+            </li>
+            <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              Service
+            </li>
+            </motion.ul>
+
+          <div
+            className="relative flex items-center justify-center"
+            // onClick={showCart}
+            >
+                <MdShoppingBasket className="text-textColor text-2xl  cursor-pointer"/>
+                <div className=" absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+                <p className="text-xs text-white font-semibold">
+                2
+                </p>
+              </div>
+            </div>
+
+
+            </div>
+            </div>
+
+
+
+
         
         <div className='flex md:hidden w-full h-full'></div>
-    </div>
+    </header>
   )
 }
 
