d1d30f070cd690da8c4da126440f781d243534e2
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-10 23:16:20 +0530

    re

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <StateProvider initialState={initialState}>
     <App />
+    </StateProvider>
   </BrowserRouter>
 );
 
