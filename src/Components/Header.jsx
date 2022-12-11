bf989c814351f58e7792eb8dd96b29010a1dcac9
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-11 12:28:45 +0530

    drop down menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -22,6 +22,7 @@ const Header = () => {
     const [{user}, dispatch] = useStateValue()
 
     const login = async () => {
+       if (!user) {
         const {
           user: {refreshToken, providerData},
         } = await signInWithPopup(firebaseAuth, provider)
@@ -29,6 +30,8 @@ const Header = () => {
           type: actionType.SET_USER,
           user : providerData[0],
         })
+        localStorage.setItem('user', JSON.stringify(providerData[0]));
+       }
     }
 
   return (
@@ -86,9 +89,15 @@ const Header = () => {
                  alt="userprofile"
               onClick={login}
             />
+            <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex-col absolute top-12 right-0'>
+              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-200 ease-in-out text-textColor text-base"
+                    // onClick={() => setIsMenu(false)}
+              >New Item <MdAdd/></p>
+              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-200 ease-in-out text-textColor text-base"
+                  // onClick={logout}
+              >Logout <MdLogout/></p>
             </div>
-
-
+            </div>
             </div>
             </div>
 
--- src/Context/initialState.js
+++ src/Context/initialState.js
@@ -1,15 +1,12 @@
-// import { fetchCart, fetchUser } from "../utils/fetchLocalStorageData";
+import { fetchCart, fetchUser } from "../utils/fetchLocalStorageData";
 
-// const userInfo = fetchUser();
+const userInfo = fetchUser();
 // const cartInfo = fetchCart();
 
-// export const initialState = {
-//   user: userInfo,
-//   foodItems: null,
-//   cartShow: false,
-//   cartItems: cartInfo,
-// };
-
 export const initialState = {
-  user: null,
-}
+  user: userInfo,
+  // foodItems: null,
+  // cartShow: false,
+  // cartItems: cartInfo,
+};
+
