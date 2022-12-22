b90954c31b48b816eeae1bf631d9eae006a19537
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-22 15:08:59 +0530

    on

--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import HomeContainer from "./HomeContainer";
 import { motion } from "framer-motion";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
-// import RowContainer from "./RowContainer";
+import RowContainer from "./RowContainer";
 import { useStateValue } from "../Context/StateProvider";
 // import MenuContainer from "./MenuContainer";
 // import CartContainer from "./CartContainer";
