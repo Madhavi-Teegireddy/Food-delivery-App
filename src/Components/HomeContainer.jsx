107d916309459786c3bd30962c94e34afcbd1e26
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-22 17:21:10 +0530

    on

--- src/Components/HomeContainer.jsx
+++ src/Components/HomeContainer.jsx
@@ -61,8 +61,7 @@ const HomeContainer = () => {
               >
                 <img
                   src={n.imageSrc}
-                  // className="w-20 lg:w-40 -mt-10 lg:-mt-20 "
-                  className="w-40 -mt-20"
+                  className="w-20 lg:w-40 -mt-16 lg:-mt-20 "
                   alt="I1"
                 />
                 <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
@@ -79,6 +78,32 @@ const HomeContainer = () => {
               </div>
             ))}
         </div>
+
+        {/* <div className="w-full h-full absolute top-0 left-0 flex flex-center justify-center px-32
+        py-4 gap-4 flex-wrap">
+          {heroData &&
+          heroData.map((n) => (
+            <div
+            key={n.id}
+              className="w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center" 
+            >
+              <img src={n.imageSrc} className="w-40 -mt-20" alt="I1"/>
+              <p className="text-xl font-semibold text-textColor mt-4">
+                {n.name}
+              </p>
+
+              <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
+                  {n.decp}
+                </p>
+
+                <p className="text-sm font-semibold text-headingColor">
+                  <span className="text-xs text-red-600">$</span> {n.price}
+                </p>
+              
+            </div>
+          ))}
+
+        </div> */}
       </div>
     </section>
   );
